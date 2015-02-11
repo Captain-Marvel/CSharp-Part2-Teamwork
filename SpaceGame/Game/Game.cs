8ca41e7697fd76b28404d2fe7eb027c6f016aae5
lnikod4s <lyudmil.nikodimov@gmail.com>
2015-02-11 20:54:12 +0100

    Initial commit, added base game world

---
+++ SpaceGame/Game/Game.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SpaceGame
+{
+	class Game
+	{
+		// Set console max width and height
+		private const int maxWidth = 40;
+		private const int maxHeight = 40;
+
+		// Keeps the x coordinate of our spaceship position 
+		static readonly int playerPosX = Console.BufferWidth / 2;
+		static readonly int playerPosY = Console.BufferHeight - 1;
+
+		// Keeps x and y coordinates of enemies spaceships
+		static List<List<int>> enemies = new List<List<int>>();
+
+		// Keeps x and y coordinates of our shots
+		static List<List<int>> shots = new List<List<int>>();
+		
+		// Set spaceships and shots icons
+		private const char playerIcon = 'Ä';
+		private const char enemyIcon = '#';
+		private const char shotIcon = '"';
+
+		// Set spaceships and shots colors
+		private const ConsoleColor playerColor = ConsoleColor.Cyan;
+		private const ConsoleColor enemiesColor = ConsoleColor.DarkRed;
+		private const ConsoleColor shotsColor = ConsoleColor.DarkYellow;
+
+		// Set amount of spaceship lives
+		private const int livesCount = 5;
+
+		// Make global random variable
+		static readonly Random rnd = new Random();
+
+		static void Main()
+		{
+			Console.BufferHeight = Console.WindowHeight = maxHeight;
+			Console.BufferWidth = Console.WindowWidth = maxWidth;
+			
+			
+			//Menu with instructions
+			DrawMenuScreen();
+
+			while (livesCount >= 0)
+			{
+				// Game World
+
+				GenerateEnemyAtRandomPosition();
+
+				Update();
+
+				Draw();
+
+				if (Console.KeyAvailable)
+				{
+					// Set player game keys
+					ControlPlayer();
+				}
+
+				// After certian amount of meteors destyoed increase speed
+				IncreaseLevel(); 
+
+				Thread.Sleep(200);
+
+				Console.Clear();
+			}
+
+			// Store points per meteor hit
+			StorePoints();
+
+			// Draw final scores
+			DrawScoreBoard();
+			Console.WriteLine("You are dead! Continue (Y/N):");
+		}
+
+	
+	
+
+		private static void Update()
+		{
+			// Update Game World
+			UpdateEnemies();
+			UpdateShots();
+			CollisionsEnemiesShots();
+			CollisionsEnemiesPlayer();
+		}
+
+		private static void CollisionsEnemiesPlayer()
+		{
+			
+		}
+
+		private static void CollisionsEnemiesShots()
+		{
+			
+		}
+
+		private static void UpdateShots()
+		{
+			
+		}
+
+		private static void UpdateEnemies()
+		{
+
+		}
+
+		private static void GenerateEnemyAtRandomPosition()
+		{
+			int randomEnemyPosition = rnd.Next(0, Console.WindowWidth);
+		}
+
+		private static void Shoot()
+		{
+			
+		}
+
+		private static void Draw()
+		{
+			DrawPlayer();
+			DrawEnemies();
+			DrawShots();
+		}
+
+		private static void DrawShots()
+		{
+			
+		}
+
+		private static void DrawEnemies()
+		{
+			
+		}
+
+		private static void DrawPlayer()
+		{
+			List<int> playerCoordinates = new List<int>(){ playerPosX, playerPosY};
+			
+
+			DrawSymbolAtSpecificCoordinates(playerCoordinates, playerIcon, playerColor);
+		}
+
+		private static void DrawSymbolAtSpecificCoordinates(List<int> playerCoordinates, char c, ConsoleColor playerColor)
+		{
+			
+		}
+
+		private static void ControlPlayer()
+		{
+			ConsoleKeyInfo userInput = Console.ReadKey(true);
+			while (Console.KeyAvailable)
+			{
+				Console.ReadKey(true);
+			}
+			if (userInput.Key == ConsoleKey.Spacebar)
+			{
+				Shoot();
+			}
+			else if (userInput.Key == ConsoleKey.LeftArrow)
+			{
+				MovePlane();
+			}
+
+			else if (userInput.Key == ConsoleKey.RightArrow)
+			{
+				MovePlane();
+			}
+
+			else if (userInput.Key == ConsoleKey.UpArrow)
+			{
+				MovePlane();
+			}
+
+			else if (userInput.Key == ConsoleKey.DownArrow)
+			{
+				MovePlane();
+			}
+		}
+
+		private static void MovePlane()
+		{
+
+		}
+		private static void DrawMenuScreen()
+		{
+
+		}
+
+		private static void IncreaseLevel()
+		{
+		}
+
+		private static void DrawScoreBoard()
+		{
+		}
+
+		private static void StorePoints()
+		{
+
+		}
+	}
+}
