c0d98d4b830e9850e60ab6286b4b674feb7d0557
MartinZPetrov <martinpetrov8907@gmail.com>
2015-02-12 21:38:24 +0200

    Add IPrintable Interface

---
+++ SpaceGame/Game/IPrintable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    // All printable objects should implement this interface
+    public interface IPrintable
+    {
+        // X coordinate
+        int X { get; set; }
+        
+        // Y coordinate
+        int Y { get; set; }
+        
+        // Color of the object
+        ConsoleColor Color { get; set; }
+
+        // Printing on the console
+        void Print();
+    }
+}
